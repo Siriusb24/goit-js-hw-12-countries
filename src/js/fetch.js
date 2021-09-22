0f4451ff0e59d663a3e81598dce6eaafa376f9e2
Siriusb24 <Siriusb2324@gmail.com>
2021-09-22 12:18:28 +0300

    update_fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,7 +1,11 @@
 const BASE_URL = `https://restcountries.eu/rest/v2/name`
+const option = {
+    method : "GET",
+    mode: 'no-cors'
+}
   
 function fetchCountry(form){
-    return fetch(`${BASE_URL}/${form}`).then(res => res.json());
+    return fetch(`${BASE_URL}/${form}`, option).then(res => res.json());
 } 
 
 export default {fetchCountry}
